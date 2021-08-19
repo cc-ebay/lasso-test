6c1acb564ff97c96f80d7fd3aa26d0aa026e7c3e
Chris Cinelli <ccinelli@ebay.com>
2021-08-18 19:22:23 -0700

    Adding another route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import { configure } from "lasso";
 import { serveStatic } from "lasso/middleware";
 import markoMiddleware from "@marko/express";
 import homePage from "./pages/home";
+import homePage2 from "./pages/home2";
 
 // Configure lasso to control how JS/CSS/etc. is delivered to the browser
 const isProduction = process.env.NODE_ENV === "production";
@@ -32,6 +33,7 @@ express()
   .use(markoMiddleware()) // Enables res.marko
   .use(serveStatic()) // Serve static assets with lasso
   .get("/", homePage) // Setup the route for our home page handler
+  .get("/2", homePage2) // Setup the route for our home page handler
   .listen(process.env.PORT || 8080, function () {
     console.log(
       "Server started! Try it out:\nhttp://localhost:" +
